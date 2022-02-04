3f9e2eaada4b33dcb05d5a2cee3b856294c97981
thongvm-vand <minh.thong@vand.vn>
2022-02-04 07:22:11 +0700

    finished route system

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,29 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 const Home = () => {
-  return <h1>Home Page</h1>
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log('Placing your order')
+    router.push('/products')
+  }
+
+  return (
+    <>
+      <h1>Home Page</h1>
+
+      <Link href='/blog'>
+        <a>Blog</a>
+      </Link>
+
+      <hr />
+      <Link href='/products'>
+        <a>Products</a>
+      </Link>
+
+      <button onClick={handleClick}>Place Order</button>
+    </>
+  )
 }
 
 export default Home
